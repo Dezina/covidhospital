f25d9bcf8590637f3ceb31e96d1fcf06b9b1cc8c
Dezina <50049909+Dezina@users.noreply.github.com>
2020-05-04 13:37:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = Number(process.env.PORT || 8000);
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'project18')));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'project18', 'index.html'));
+    res.sendFile(path.join(__dirname,  'index.html'));
 });
 
 app.listen(() => {
